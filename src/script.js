c0fa97c29e3a74b787119e35563965c2e6ba06da
Abdisalam <abdisalamfresh@gmail.com>
2023-08-25 01:14:14 -0700

    updating the ui of the web app

--- src/script.js
+++ src/script.js
@@ -1,15 +1,42 @@
-async function getLocation(cityName){
-    
+
+
+
+async function getWeather(cityName){
+
     try{
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=8cced427c682481685b222916231703&q=${cityName} &aqi=no`);
 
         if(response.ok){
             const data = response.json();
             return data;
+        }else {
+            throw new Error("Location Not Found")
         }
     }catch (error){
-        console.log("Error fetching: ", error);
-        return null;
+            console.log("Error fetching: ", error);
+            return null;
+    }
+    
+
+}
+
+function displayWeather(e){
+    e.preventDefault();
+    
+    const userInput = document.getElementById("userInput")
+
+    if(userInput.value !== ""){
+        getWeather(userInput.value)
+            .then(data => {
+                console.log(data)
+                const {name, region, country, localtime} = data.location;
+                const {feelslike_f, temp_f, wind_mph, humidity, condition} = data.current;
+
+                console.log(name, region)
+                return {name, region, country, localtime};
+        })
+    }else {
+        alert("U must enter something")
     }
     
 
@@ -17,51 +44,5 @@ async function getLocation(cityName){
 
 
 
-getLocation("Philippines")
-    .then(data => {
-        console.log(data)
-    })
-
-// async function getWeather(lat, lon){
-//     return fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,rain,visibility,${"is_day"}&current_weather=true&models=best_match`)
-//                 .then(res => {
-//                     if(res.ok){
-//                         return res.json();
-//                     }else {
-//                         throw new Error("Error res not Ok")
-//                     }
-//                 })
-//                 .then(data => {
-//                     getLocation()
-//                             .then(data => {
-//                                 console.log(data)
-//                             })
-//                 })
-//                 .catch(error => {
-//                     console.log("Error fetching: ", error)
-//                 })
-// }
-
-// getWeather();
-
-// const apiKey = '44620723fec33f6cad2a598c4d6cc9f1';
-
-// async function getWeather() {
-//   try {
-//     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=-1.2833&longitude=36.8167&hourly=temperature_2m`);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error('Error fetching:', error);
-//   }
-// }
-
-// getWeather();
-
-
-
-
-
+const submitBtn = document.getElementById('submit')
+submitBtn.addEventListener('click', displayWeather)
